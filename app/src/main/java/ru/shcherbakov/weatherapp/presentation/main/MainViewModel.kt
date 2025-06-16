a951efab6b28a78178de2d231e76a8c5dc88915e
Ilya <kapter337@yandex.ru>
2025-06-16 17:10:33 +0300

    Material lib updated, fonts added, fragment_main.xml done

--- app/src/main/java/ru/shcherbakov/weatherapp/presentation/main/MainViewModel.kt
+++ app/src/main/java/ru/shcherbakov/weatherapp/presentation/main/MainViewModel.kt
@@ -1,14 +1,8 @@
 package ru.shcherbakov.weatherapp.presentation.main
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.launch
-import ru.shcherbakov.weatherapp.data.FactListRepositoryImpl
 import ru.shcherbakov.weatherapp.data.FactService
-import ru.shcherbakov.weatherapp.domain.Fact
 import javax.inject.Inject
 
 @HiltViewModel
